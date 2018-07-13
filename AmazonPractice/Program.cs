78d73aa429ac9f5a769988f7f1c69da1526f27fe
Vince Lee <vlee@BES.blackstoneenergy.com>
2018-07-13 13:06:03 -0400

    Implemented binary tree Implemented vertical summing

--- AmazonPractice/Program.cs
+++ AmazonPractice/Program.cs
@@ -10,6 +10,53 @@ namespace AmazonPractice
     {
         static void Main(string[] args)
         {
+            //Construct tree
+            BinaryTree<int> bTree = new BinaryTree<int>();
+
+            bTree.Root = new BinaryTreeNode<int>(1);
+            bTree.Root.Left = new BinaryTreeNode<int>(2);
+            bTree.Root.Right = new BinaryTreeNode<int>(3);
+            bTree.Root.Left.Left = new BinaryTreeNode<int>(4);
+            bTree.Root.Left.Right = new BinaryTreeNode<int>(5);
+            bTree.Root.Right.Right = new BinaryTreeNode<int>(6);
+            bTree.Root.Right.Left = new BinaryTreeNode<int>(7);
+
+            //Transverse and assign to hashmap
+            VerticalSum(bTree);
+        }
+
+        /// <summary>
+        /// Prints out the vertical sum of each vertical line
+        /// </summary>
+        private static void VerticalSum(BinaryTree<int> bTree)
+        {
+            if (bTree.Root == null)
+                return;
+
+            Dictionary<int, int> hashMap = new Dictionary<int, int>();
+
+            TransverseAndMap(hashMap, 0, bTree.Root);
+
+            foreach (var entry in hashMap)
+            {
+                Console.WriteLine("Line/Value: {0} / {1}",
+                    entry.Key, entry.Value);
+            }
+        }
+
+        private static void TransverseAndMap(Dictionary<int, int> hashMap, int verticalPosition, BinaryTreeNode<int> root)
+        {
+            if (hashMap == null || root == null)
+                return;
+
+            TransverseAndMap(hashMap, verticalPosition - 1, root.Left);
+
+            if (hashMap.ContainsKey(verticalPosition))
+                hashMap[verticalPosition] += root.Value;
+            else
+                hashMap.Add(verticalPosition, root.Value);
+
+            TransverseAndMap(hashMap, verticalPosition + 1, root.Right);
         }
     }
 }
